91b394f8cc46d982e0e40bc351e25fe879efe844
Avwerosuo25 <kelvinklein425@gmail.com>
2023-04-27 00:55:17 -0700

    Client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,16 @@
-# Set the config of a ssh_file
-class ssh_config {
-  ssh::client::config { '/etc/ssh/ssh_config':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    match  => [
-      {
-        # Configure the host you want to connect to without a password
-        host     => 'your.server.com',
-        options  => {
-          'IdentityFile' => '~/.ssh/school',
-          'PasswordAuthentication' => 'no',
-        },
-      },
-    ],
-  }
+# Configure client SSH to use private key and disable password authentication
+include stdlib
+
+file_line { 'Disable Password Authentication':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Use Private Key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
